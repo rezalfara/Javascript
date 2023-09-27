cfc4a4a976b70db8b769dbcccedbc3166b19d9c7
rezalfara <reza.alfara07@gmail.com>
2023-09-27 17:17:25 +0700

    commit

--- Javascript Fundamental/main.js
+++ Javascript Fundamental/main.js
@@ -27,9 +27,65 @@ let nilai = 9
 //Null coalescing
 //undefined dan null dianggap tidak ada nilai
 //di cek dari yang paling kiri
-let a = undefined
-let b = null
-let c = 7
+// let a = undefined
+// let b = null
+// let c = 7
 
-let hasil = a ?? b ?? c ?? 'Whatever'
-alert(hasil)
+// let hasil = a ?? b ?? c ?? 'Whatever'
+// alert(hasil)
+
+
+// Function declaration
+// function first() {
+//     alert('Hello World')
+// }
+
+// first()
+
+
+// Function dengan parameter
+// function luasPersegiPanjang(panjang, lebar) {
+//     alert(panjang * lebar)
+// }
+
+// luasPersegiPanjang(5, 4)
+
+
+// Local & Global variable
+// let global = 'ini global variable'
+
+// function tesFunction() {
+//     let local = 'ini local variable'
+// }
+
+// tesFunction(alert(global))
+
+
+// Default value parameter
+// function hitungLuasPersegi(panjang, lebar = 2) {
+//     alert(panjang * lebar)
+// }
+
+// hitungLuasPersegi(5)
+
+
+// Function return value
+// function hitungLuasPersegi(panjang, lebar = 2) {
+//     let hasil = panjang * lebar
+//     return hasil
+// }
+
+// let persegiPanjang1 = hitungLuasPersegi(5, 4)
+// let persegiPanjang2 = hitungLuasPersegi(3)
+
+// alert('Persegi panjang 1 luasnya : ' + persegiPanjang1)
+// alert('Persegi panjang 2 luasnya : ' + persegiPanjang2)
+
+
+// Function expression (bisa menggunakan parameter juga)
+// function expression tidak bisa dipanggil diatas functionnya, sedangkan function declaration bisa
+// let fungsi2 = function(){
+//     alert('This is function expression')
+// }
+
+// fungsi2()
